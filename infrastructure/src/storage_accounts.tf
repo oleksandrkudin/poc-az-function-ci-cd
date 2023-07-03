29ac69a6975851951fc772a24e63d8956acb91ab
Oleksandr Kudin <oleksandr.kudin@gmail.com>
2023-07-03 18:41:34 +0300

    bugfix - fix storage account name in instrastructure

--- infrastructure/src/storage_accounts.tf
+++ infrastructure/src/storage_accounts.tf
@@ -1,5 +1,5 @@
 resource "azurerm_storage_account" "sa" {
-  name                     = replace(format("%s-%s", "funcappsa"), "-", "")
+  name                     = replace(format("%s-%s", var.name_prefix, "funcappsa"), "-", "")
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
   account_tier             = "Standard"
