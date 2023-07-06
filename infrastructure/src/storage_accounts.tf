2db507ee6786cd6ead12c7025237f62f32ba8c67
Oleksandr Kudin <oleksandr.kudin@gmail.com>
2023-07-06 09:01:45 +0300

    Set storage account kind to Storage but not StorageV2

--- infrastructure/src/storage_accounts.tf
+++ infrastructure/src/storage_accounts.tf
@@ -2,6 +2,7 @@ resource "azurerm_storage_account" "sa" {
   name                     = replace(format("%s-%s", var.name_prefix, "funcappsa"), "-", "")
   resource_group_name      = azurerm_resource_group.rg.name
   location                 = var.location
+  account_kind             = "Storage"
   account_tier             = "Standard"
   account_replication_type = "LRS"
 }
