0e430d480ba7a4ac090d6cf6adaef24ced566810
Oleksandr Kudin <oleksandr.kudin@gmail.com>
2023-07-06 09:20:34 +0300

    Disable use_dotnet_isolated_runtime, https_only in site config of function app

--- infrastructure/src/function_apps.tf
+++ infrastructure/src/function_apps.tf
@@ -7,13 +7,13 @@ resource "azurerm_linux_function_app" "example" {
   storage_account_access_key = azurerm_storage_account.sa.primary_access_key
   service_plan_id            = azurerm_service_plan.function_plan.id
 
-  https_only = true
+  # https_only = true
   functions_extension_version = "~4"
 
   site_config {
     application_stack {
       dotnet_version = "6.0"
-      use_dotnet_isolated_runtime = true
+      use_dotnet_isolated_runtime = false
     }
     use_32_bit_worker = true
     ftps_state = "FtpsOnly"
