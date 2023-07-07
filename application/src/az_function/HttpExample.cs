f00fa77fbc4d08bd9b5b29100e36b6c1885c7f49
Oleksandr Kudin <oleksandr.kudin@gmail.com>
2023-07-07 12:51:27 +0300

    Embrace function_app_names value in debug step

--- application/src/az_function/HttpExample.cs
+++ application/src/az_function/HttpExample.cs
@@ -22,7 +22,7 @@ namespace az_function
             var response = req.CreateResponse(HttpStatusCode.OK);
             response.Headers.Add("Content-Type", "text/plain; charset=utf-8");
 
-            response.WriteString("Welcome Oleksandr Kudin to Azure Functions version 4!");
+            response.WriteString("Welcome Oleksandr Kudin to Azure Functions version 5!");
 
             return response;
         }
